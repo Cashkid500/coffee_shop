61ea097407640c1703dd2fa0735338cf0844b0b9
Cashkid500 <Murtadhob@gmail.com>
2023-08-12 16:51:37 +0100

    Color constant variables created to avoid future modifications by an unauthorized person

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// Onboarding Colors //
+Color blackText = const Color(0xff000000);
+Color whiteText = const Color(0xffFFFFFF);
+Color darkGrayText = const Color(0xffA9A9A9);
+
+// Dashboard Colors //
+Color sealBrownText = const Color(0xff131313);
+Color spanishGrayText = const Color(0xff989898);
+Color nightRiderText = const Color(0xff313131);
+Color pinkText = const Color(0xffC67C4E);
+Color whiteSmokeText = const Color(0xffF3F3F3);
+Color darkGray2Text = const Color(0xff9B9B9B);
+Color deepBrownText = const Color(0xff2F4B4E);
+
+// Detail Screen Colors //
+Color darkCharcoalText = const Color(0xff2F2D2C);
+Color grayText = const Color(0xff808080);
+Color white2Text = const Color(0xffFFF0F0);
+Color whisperText = const Color(0xffEAEAEA);
+Color gray87Text = const Color(0xffDEDEDE);
+Color seaShellText = const Color(0xffFFF5EE);
+Color pinkButton = const Color(0xffC67C4E);
+
+// Order Screen Colors //
+Color lightRedBox = const Color(0xffF0F0F0);
+Color unknownColorText = const Color(0xff303336);
+Color gray87Box = const Color(0xffDEDEDE);
+Color whiteBox = const Color(0xffFFFFFF);
