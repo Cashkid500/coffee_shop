a5b6607ee8722fe99917da64eb6c3d0813a20b24
Cashkid500 <Murtadhob@gmail.com>
2023-08-13 00:30:49 +0100

    Added texts constants to avoid modifications in the future by an unauthorized person

---
+++ lib/constants/text_constants.dart
@@ -0,0 +1,58 @@
+class CoffeeShopText {
+  // onboarding Texts //
+  static const String onboardingText =
+      'Coffee so good, your taste buds will love it.';
+  static const String onboarding2Text =
+      'The best grain, the finest roast, the powerful flavor.';
+  static const String googleMessage = 'Continue with Google';
+
+  // Dashboard Texts //
+  static const String capText = 'Cappuccino';
+  static const String machText = 'Machiato';
+  static const String lattText = 'Latte';
+  static const String ameText = 'Americano';
+  static const String locText = 'Location';
+  static const String bilText = "Bilzen,Tanjungbalai";
+  static const String searchText = 'Search coffee';
+  static const String fourPointEightText = '4.8';
+  static const String withChocolateText = 'with Chocolate';
+  static const String capPriceText = "\$ 4.53";
+  static const String fourPointFiveText = '4.5';
+  static const String fourPointZeroText = '4.0';
+
+  // Detail Screen Texts //
+  static const String detText = "Detail";
+  static const String cap2Text = "Cappucino";
+  static const String bracText = "(230)";
+  static const String descripText = "Description";
+  static const String capLongText =
+      "A cappuccino is an approximately 150 ml (5 oz) beverage, with 25 ml of espresso coffee and 85ml of fresh milk the fo.. ";
+  static const String readMoreText = "Read More";
+  static const String sizeText = "Size";
+  static const String sText = "S";
+  static const String mText = "M";
+  static const String lText = "L";
+  static const String priceText = "Price";
+  static const String buttonText = "Buy Now";
+
+  // Order Screen Texts //
+  static const String ordText = "Order";
+  static const String delivText = "Deliver";
+  static const String pickUpText = "Pick Up";
+  static const String delivAddText = "Delivery Address";
+  static const String houseOwnersNameText = "JI. Kpg Sutoyo";
+  static const String addressNameText =
+      "Kpg. Sutoyo No. 620, Bilzen, Tanjungbalai.";
+  static const String editAddressText = "Edit Address";
+  static const String addNoteText = "Add Note";
+  static const String oneText = "1";
+  static const String discountText = "1 Discount is applied";
+  static const String paySummaryText = "Payment Summary";
+  static const String delivFeeText = "Delivery Fee";
+  static const String textSpanText = "\$ 2.0  ";
+  static const String textSpan2Text = "\$ 1.0";
+  static const String totalPaymentText = "Total Payment";
+  static const String totalPriceText = "\$ 5.53";
+  static const String cashText = 'Cash';
+  static const String orderButtonText = 'Order';
+}
