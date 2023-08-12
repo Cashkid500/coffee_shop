a8225d6e628f2c8ddf40446a86bb337a50591e13
Cashkid500 <Murtadhob@gmail.com>
2023-08-13 00:28:19 +0100

    Added fonts constant to improve code readability and maintainability

--- lib/constants/asset_path.dart
+++ lib/constants/asset_path.dart
@@ -1,49 +1,54 @@
+import 'package:flutter/material.dart';
+
 class CoffeeShopAssetsPath {
-    static const iconsPath = 'assets/icons';
-    static const imagesPath = 'assets/images';
-    static const logosPath = 'assets/logos';
-    static const fontsPath = 'assets/fonts';
-    
+  static const iconsPath = 'assets/icons';
+  static const imagesPath = 'assets/images';
+  static const logosPath = 'assets/logos';
+  static const fontsPath = 'assets/fonts';
 
-    // Onboarding Images //
-    static const coffeeimage = 'assets/images/Coffee.png';
-    static const googleimage = 'assets/images/Google.png';
+  // Order Screen Icons //
+  static const Icon add = Icon(Icons.add, size: 18,);
+  static const Icon remove = Icon(Icons.remove, size: 18,);
 
-    // Dashboard Images //
-    static const arrowDown = 'assets/images/Arrow Down.png';
-    static const girlImage = 'assets/images/Girl.png';
-    static const searchImage = 'assets/images/Search.png';
-    static const frame10 = 'assets/images/Frame 10.png';
-    static const frame8 = 'assets/images/Frame 8.png';
-    static const cappucinoImage = 'assets/images/Cappucino.png';
-    static const starImage = 'assets/images/Star.png';
-    static const frame17 = 'assets/images/Frame 17.png';
-    static const cappucino2 = 'assets/images/Cappucino 2.png';
-    static const cappucino3 = 'assets/images/Cappucino 3.png';
-    static const cappucino4 = 'assets/images/Cappucino 4.png';
-    static const homeImage = 'assets/images/Home.png';
-    static const heartImage = 'assets/images/Heart.png';
-    static const bagImage = 'assets/images/Bag.png';
-    static const notificationImage = 'assets/images/Notification.png';
+  // Onboarding Images //
+  static const coffeeimage = 'assets/images/Coffee.png';
+  static const googleimage = 'assets/images/Google.png';
 
-    // Detail Images //
-    static const arrowImage = 'assets/images/Arrow.png';
-    static const heart2 = 'assets/images/Heart 2.png';
-    static const cappucino5 = 'assets/images/Cappucino 5.png';
-    static const ratingImage = 'assets/images/Rating.png';
-    static const frame19 = 'assets/images/Frame 19.png';
-    static const frame20 = 'assets/images/Frame 20.png';
+  // Dashboard Images //
+  static const arrowDown = 'assets/images/Arrow Down.png';
+  static const girlImage = 'assets/images/Girl.png';
+  static const searchImage = 'assets/images/Search.png';
+  static const frame10 = 'assets/images/Frame 10.png';
+  static const frame8 = 'assets/images/Frame 8.png';
+  static const cappucinoImage = 'assets/images/Cappucino.png';
+  static const starImage = 'assets/images/Star.png';
+  static const frame17 = 'assets/images/Frame 17.png';
+  static const cappucino2 = 'assets/images/Cappucino 2.png';
+  static const cappucino3 = 'assets/images/Cappucino 3.png';
+  static const cappucino4 = 'assets/images/Cappucino 4.png';
+  static const homeImage = 'assets/images/Home.png';
+  static const heartImage = 'assets/images/Heart.png';
+  static const bagImage = 'assets/images/Bag.png';
+  static const notificationImage = 'assets/images/Notification.png';
 
-    // Order Images //
-    static const editImage = 'assets/images/Edit.png';
-    static const noteImage = 'assets/images/Note.png';
-    static const cappucino6 = 'assets/images/Cappucino 6.png';
-    static const discountImage = 'assets/images/Discount.png';
-    static const arrow2 = 'assets/images/Arrow 2.png';
-    static const moneyImage = 'assets/images/Money.png';
-    static const vectorImage = 'assets/images/Vector.png';
+  // Detail Images //
+  static const arrowImage = 'assets/images/Arrow.png';
+  static const heart2 = 'assets/images/Heart 2.png';
+  static const cappucino5 = 'assets/images/Cappucino 5.png';
+  static const ratingImage = 'assets/images/Rating.png';
+  static const frame19 = 'assets/images/Frame 19.png';
+  static const frame20 = 'assets/images/Frame 20.png';
 
-    // Fonts //
-    static const soraFont = 'Sora';
-    static const robotoFont = 'Roboto';
+  // Order Images //
+  static const editImage = 'assets/images/Edit.png';
+  static const noteImage = 'assets/images/Note.png';
+  static const cappucino6 = 'assets/images/Cappucino 6.png';
+  static const discountImage = 'assets/images/Discount.png';
+  static const arrow2 = 'assets/images/Arrow 2.png';
+  static const moneyImage = 'assets/images/Money.png';
+  static const vectorImage = 'assets/images/Vector.png';
+
+  // Fonts //
+  static const soraFont = 'Sora';
+  static const robotoFont = 'Roboto';
 }
